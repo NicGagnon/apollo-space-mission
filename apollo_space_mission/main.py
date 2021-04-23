f0fb4b30349909b0f17e81a46d2e2f9910c4b3e7
Nicolas Gagnon <nicolas.gagnon@prestacap.com>
2021-04-23 08:53:25 +0200

    dask batch processing for large dataset

--- apollo_space_mission/main.py
+++ apollo_space_mission/main.py
@@ -2,7 +2,9 @@ from argparse import ArgumentParser
 import sys
 import pandas as pd
 from pathlib import Path
-
+from glob import glob
+import pyarrow.parquet as pq
+import dask.dataframe as dd
 
 def parse_config():
     my_args = ArgumentParser('ApolloSpaceMission')
@@ -10,7 +12,7 @@ def parse_config():
     return my_args.parse_args(sys.argv[1:])
 
 
-def address_changed(target_client):
+def did_address_changed(target_client):
     hit_data = target_client.get('hit')
     lats, longs = [], []
     for hit in hit_data:
@@ -23,11 +25,20 @@ def address_changed(target_client):
 
 
 def get_client_frontendid(target_client):
-    pass
+    hit_data = target_client.get('hit')
+    for hit in hit_data:
+        custom_dimensions = hit.get('customDimensions', [])
+        frontEndId = [list(cd.values())[-1] for cd in custom_dimensions if '36' in cd.values()]
+        if len(frontEndId) > 0:
+            return frontEndId[0]
+    return -1
+
+def load_data(folder_path):
+    data_dir = Path(__file__).parent.joinpath('data', folder_path)
+    full_df = dd.read_parquet(data_dir, engine='pyarrow')
+    return full_df
 
 
-def get_client_app_type(target_client):
-    pass
 
 
 def examine_visitor(vid):
@@ -53,29 +64,27 @@ def examine_visitor(vid):
         "app_type": "n/a"
     }
 
-    data_dir = Path(__file__).parent.joinpath('data')
-
     # input validation
     if vid is None:
         return client_info
-    if isinstance(vid, str) and vid.isdigit:
-        vid = int(vid)
+    #if isinstance(vid, str) and vid.isdigit:
+    #    vid = int(vid)
 
     # Load the ga-session data as DataFrame
-    ga_df = pd.read_json(data_dir.joinpath('ga-sessions.json'), orient='records')
-    client_data = ga_df[ga_df.fullvisitorid == vid]
-
+    ga_df = load_data('ga-sessions')
+    #todo sort by longest session time or largest session time but still only one record
+    client_df = ga_df[ga_df.fullvisitorid == vid]
     # If no client data, then return default client info since id doesn't exist
-    if client_data.empty:
+    if len(client_df.index) == 0:
         return client_info
 
     # Fill information related to ga-session dataset
-    target_client = client_data.to_dict(orient='records')[0]
-    client_info['add_changed'] = address_changed(target_client)
-    client_info['app'] = get_client_app_type(target_client)
+    target_client = client_df.compute().to_dict(orient='records')[0]
+    client_info['add_changed'] = did_address_changed(target_client)
+    client_info['app'] = target_client.get('operatingSystem', 'not found')
 
     # Fill information related to transaction dataset
-    td_df = pd.read_json(data_dir.joinpath('transactionData.json'), orient='records')
+    td_df = load_data('transaction-data')
     fid = get_client_frontendid(target_client)
     trx_data = td_df[td_df.frontendOrderId == fid]
     client_info['order_placed'] = trx_data.empty
